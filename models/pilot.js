41df7a1ef98b76ebaaab8df9b7d53986548d9575
puppycodes <woof@puppy.codes>
2017-07-05 09:00:42 -0700

    change bcrypt to bcryptjs for compatibility with osx

--- models/pilot.js
+++ models/pilot.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Ride = require('./ride');
 
 // Use native promises.
