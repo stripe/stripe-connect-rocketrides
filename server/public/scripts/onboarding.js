f25d96157d0969000978bca37ae5f7d6ddf92b99
Mat Basta <basta@stripe.com>
2017-10-13 10:39:43 -0700

    Clean up onboarding.js

--- server/public/scripts/onboarding.js
+++ server/public/scripts/onboarding.js
@@ -2,13 +2,17 @@ document.body.addEventListener('change', function(e) {
   if (e.target.name !== 'pilot-type') {
     return;
   }
+  // Show the correct header for legal entity information.
   document.querySelector('.type-header.is-visible').classList.toggle('is-visible');
   document.querySelector(`.type-header.${e.target.value}-info`).classList.add('is-visible');
 
-  Array.prototype.slice.call(document.querySelectorAll('.pilot-info')).forEach(function(elem) {
-    elem.classList.remove('is-visible');
-  });
-  Array.prototype.slice.call(document.querySelectorAll(`.pilot-info.${e.target.value}-type`)).forEach(function(elem) {
-    elem.classList.add('is-visible');
+  // Show any fields that apply to the new pilot type.
+  Array.prototype.slice.call(
+    document.querySelectorAll('.pilot-info')
+  ).forEach(function(elem) {
+    elem.classList.toggle(
+      'is-visible',
+      elem.classList.contains(`${e.target.value}-type`)
+    );
   });
 });
