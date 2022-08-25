1c8c6b15e625e263400d25dc78bbc0fb581a62d3
mpark-stripe <110872953+mpark-stripe@users.noreply.github.com>
2022-08-25 13:35:50 -0700

    Add Stripe API version to config (#76)
    Adds Stripe API version to the config file. All Stripe calls are pinned to this API version, with the default being 2022-08-01 if not set.

--- server/models/passenger.js
+++ server/models/passenger.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const config = require('../config');
-const stripe = require('stripe')(config.stripe.secretKey);
+const stripe = require('stripe')(config.stripe.secretKey, {
+  apiVersion: config.stripe.apiVersion || '2022-08-01'
+});
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
